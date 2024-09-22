fd83dfd6c354bef5a56063db692b20b251e09466
Android9i8 <andreikrsh@gmail.com>
2024-09-22 16:10:37 +0300

    ТЗ

--- test_calculator.py
+++ test_calculator.py
@@ -1,5 +1,9 @@
 from calculator import Calculator
 
+# Задание
+# 1. Разаботчику 1 - реализовать функцию умножения
+# 1. Разаботчику 2 - реализовать функцию деления
+
 calc = Calculator()
 calc.sum(10, 5)
 calc.print_last_res()
